f1dc37b95411602f262d0b7041dde17c4c981e0e
leeyoungchun <dldydcns123@gmail.com>
2022-02-06 20:11:44 +0900

    로그 추적기 v2 - 실제 서비스에 적용

---
+++ src/main/java/spring/advanced/app/v2/OrderControllerV2.java
@@ -0,0 +1,30 @@
+package spring.advanced.app.v2;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import spring.advanced.trace.TraceStatus;
+import spring.advanced.trace.hellotrace.HelloTraceV1;
+import spring.advanced.trace.hellotrace.HelloTraceV2;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+    private final HelloTraceV2 trace;
+
+    @GetMapping("/v2/request")
+    public String request(String itemId){
+        TraceStatus status = null;
+        try{
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+            return "OK";
+        }catch (Exception e){
+            trace.exception(status, e);
+            throw e; //예외를 꼭 다시 던저주어야한다.
+        }
+    }
+}
