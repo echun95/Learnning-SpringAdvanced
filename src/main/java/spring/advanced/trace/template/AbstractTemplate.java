4630a347ee47af865ca7050410c192cad0f49dae
leeyoungchun <dldydcns123@gmail.com>
2022-02-08 21:28:26 +0900

    실제 서비스에 익명 내부클래스로 템플릿 메서드 패턴 적용

---
+++ src/main/java/spring/advanced/trace/template/AbstractTemplate.java
@@ -0,0 +1,29 @@
+package spring.advanced.trace.template;
+
+import spring.advanced.trace.TraceStatus;
+import spring.advanced.trace.logtrace.LogTrace;
+
+public abstract class AbstractTemplate<T> {
+
+    private final LogTrace trace;
+
+    public AbstractTemplate(LogTrace trace){
+        this.trace = trace;
+    }
+
+    public T execute(String message){
+        TraceStatus status = null;
+        try {
+            status = trace.begin(message);
+            //로직 호출
+            T result = call();
+            trace.end(status);
+            return result;
+        }catch (Exception e){
+            trace.exception(status,e);
+            throw e; //예외를 꼭 던져주어야한다.
+        }
+    }
+
+    protected abstract T call();
+}
