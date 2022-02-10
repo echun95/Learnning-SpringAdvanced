ea083b1f577e1d805ff44b38249ab54dd65fc203
leeyoungchun <dldydcns123@gmail.com>
2022-02-10 19:19:01 +0900

    실제 서비스에 템플릿 콜백 패턴 적용

---
+++ src/main/java/spring/advanced/app/v5/OrderControllerV5.java
@@ -0,0 +1,32 @@
+package spring.advanced.app.v5;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import spring.advanced.trace.callback.TraceCallback;
+import spring.advanced.trace.callback.TraceTemplate;
+import spring.advanced.trace.logtrace.LogTrace;
+import spring.advanced.trace.template.AbstractTemplate;
+
+@RestController
+public class OrderControllerV5 {
+
+    private final OrderServiceV5 orderService;
+    private final TraceTemplate temptlate;
+
+    public OrderControllerV5(OrderServiceV5 orderService, LogTrace trace) {
+        this.orderService = orderService;
+        this.temptlate = new TraceTemplate(trace);
+    }
+
+    @GetMapping("/v5/request")
+    public String request(String itemId){
+        return temptlate.execute("OrderController.request()", new TraceCallback<>() {
+            @Override
+            public String call() {
+                orderService.orderItem(itemId);
+                return "OK";
+            }
+        });
+    }
+}
---
+++ src/main/java/spring/advanced/app/v5/OrderServiceV5.java
@@ -0,0 +1,27 @@
+package spring.advanced.app.v5;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import spring.advanced.trace.callback.TraceCallback;
+import spring.advanced.trace.callback.TraceTemplate;
+import spring.advanced.trace.logtrace.LogTrace;
+import spring.advanced.trace.template.AbstractTemplate;
+
+@Service
+public class OrderServiceV5 {
+
+    private final OrderRepositoryV5 orderRepository;
+    private final TraceTemplate template;
+
+    public OrderServiceV5(OrderRepositoryV5 orderRepository, LogTrace trace) {
+        this.orderRepository = orderRepository;
+        this.template = new TraceTemplate(trace);
+    }
+
+    public void orderItem(String itemId){
+        template.execute("OrderService.orderItem()", () -> {
+            orderRepository.save(itemId);
+            return null;
+        });
+    }
+}
